1a3ae2f496da3231e6bb11b6b1c453a2a7c5ad2f
Alpri Else <aelse@hubspot.com>
2019-09-08 22:12:25 -0400

    Add actual email to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css'
 
 const Footer = () => (
   <footer className={classNames('text-center fontOpenSans', styles.footer)}>
-    <p className='text-black-50'>Organized by the Philippine Student Association at the University of Illinois at Urbana-Champaign.<br/>Questions? Interested in sponsoring? Email us at <a href="mailto:someone@yoursite.com?subject=Mail from Our Site">someemail@gmail.com</a></p>
+    <p className='text-black-50'>Organized by the Philippine Student Association at the University of Illinois at Urbana-Champaign.<br/>Questions? Interested in sponsoring? Email us at <a href="mailto:fact@psauiuc.org?subject=Mail from Our Site">fact@psauiuc.org</a></p>
   </footer>
 )
 export default Footer
