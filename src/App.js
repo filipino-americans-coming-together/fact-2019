b7e989e42eae1179d5f369b2ed30789834437ec4
Alpri Else <aelse@hubspot.com>
2019-09-23 20:12:35 -0400

    Add opengraph tags and twitter summary tags

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ const App = () => {
     <>
       <Router>
         <Helmet>
-          <title>FACT 2019</title>
+          
+
           <link
             rel='stylesheet'
             href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
